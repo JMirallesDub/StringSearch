7193df5fa28f4d9124e42c38a29a2698cd28855c
Tibyes <jemiralleslopez@gmail.com>
2017-10-03 14:45:51 +0100

    Initial commit

---
+++ TestSearchString/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestSearchString
+{
+
+    public static class ContainsApproaches
+    {
+        public static bool Custom(this string str, string sub)
+        {
+            return (str.Length - str.Replace(sub, String.Empty).Length) / sub.Length > 0;
+        }
+
+        public static bool Contains(this string str, string sub)
+        {
+            return str.Contains(sub);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime end;
+            DateTime start = DateTime.Now;
+
+            Console.WriteLine("### Overall Start Time: " + start.ToLongTimeString());
+            Console.WriteLine();
+
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 50);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 100);
+
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 1000);
+
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 1000);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 1000);
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 1000);
+
+            end = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("### Overall End Time: " + end.ToLongTimeString());
+            Console.WriteLine("### Overall Run Time: {0}", end - start);
+
+            Console.ReadKey();
+        }
+
+        static void TestFastestWayToSeeIfAStringOccursInAString(int NumberOfStringsToGenerate, int NumberOfSearchCharsToGenerate)
+        {
+            Console.WriteLine("######## " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            Console.WriteLine("Number of Random Strings that will be generated: {0}", NumberOfStringsToGenerate);
+            Console.WriteLine("Number of Search Strings that will be generated: {0}", NumberOfSearchCharsToGenerate);
+            Console.WriteLine();
+
+            string[] strings = GetArrayOfRandomStrings(NumberOfStringsToGenerate, 100, 150);
+            Console.WriteLine("###########################################################");
+            string[] substrings = GetArrayOfRandomStrings(NumberOfSearchCharsToGenerate, 4, 10);
+
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+
+            Console.WriteLine("T3: Substring()");
+            MeasureWithForeach(ContainsApproaches.Contains, strings, substrings);
+
+
+            Console.WriteLine("T1: Custom Substring()");
+            MeasureWithForeach(ContainsApproaches.Custom, strings, substrings);
+
+
+            GC.Collect();
+
+        }
+
+        private static void MeasureWithForeach(System.Func<string, string, bool> method, IEnumerable<string> strings, IEnumerable<string> subs)
+        {
+
+            int matches = 0;
+            var start = DateTime.Now;
+            foreach (var str in strings)
+            {
+                foreach (var sub in subs)
+                {
+                    if (method(str, sub))
+                        matches++;
+                };
+            };
+
+            var end = DateTime.Now;
+            Console.WriteLine("Result : {0}", end - start);
+            Console.WriteLine("Matches: {0}", matches);
+            Console.WriteLine();
+        }
+
+        private static string[] GetArrayOfRandomStrings(int count, int min_len, int max_len)
+        {
+            var res = new string[count];
+            var d = max_len - min_len;
+            if (d <= 0)
+                d = 10;
+            for (int x = 0; x < count; x++)
+            {
+                int len = Randomizer.Next(min_len, max_len);
+                res[x] = AppendRandom("", len, "abcdefghijklmnopqrstuvwzyz 1234567890");
+            }
+
+            return res;
+        }
+
+        private static Random Randomizer = new Random();
+        public static string AppendRandom(string src, int min_length, string char_domain = null)
+        {
+            int src_len = (string.IsNullOrEmpty(src)) ? 0 : src.Length;
+
+            if (src_len >= min_length)
+                return src;
+
+            if ("" == char_domain && min_length > src_len)
+                throw new ArgumentException("Can not append chars from empty set. Pass at least one char to char_domain", "char_domain");
+
+            if (null == char_domain)
+                char_domain = "absadfsadfsadfasdf";
+
+            int len = char_domain.Length;
+
+            if (null == src)
+                src = "";
+
+            while (src_len < min_length)
+            {
+                src += char_domain[Randomizer.Next(len)];
+                src_len++;
+            }
+
+            return src;
+        }
+    }
+
+}
---
+++ TestSearchString/ProgramOriginal.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestSearchString
+{
+    class ProgramOriginal
+    {
+        static void MainOriginal(string[] args)
+        {
+            DateTime end;
+            DateTime start = DateTime.Now;
+
+            Console.WriteLine("### Overall Start Time: " + start.ToLongTimeString());
+            Console.WriteLine();
+
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 1);
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 100);
+            TestFastestWayToSeeIfAStringOccursInAString(5000, 1000);
+            TestFastestWayToSeeIfAStringOccursInAString(25000, 1000);
+            TestFastestWayToSeeIfAStringOccursInAString(100000, 1000);
+            TestFastestWayToSeeIfAStringOccursInAString(1000000, 1000);
+
+            end = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("### Overall End Time: " + end.ToLongTimeString());
+            Console.WriteLine("### Overall Run Time: " + (end - start));
+
+            Console.WriteLine();
+            Console.WriteLine("Hit Enter to Exit");
+            Console.ReadLine();
+
+        }
+
+        //###############################################################
+
+        static void TestFastestWayToSeeIfAStringOccursInAString(int NumberOfStringsToGenerate, int NumberOfSearchCharsToGenerate)
+        {
+            Console.WriteLine("######## " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            Console.WriteLine("Number of Random Strings that will be generated: " + NumberOfStringsToGenerate.ToString("#,##0"));
+            Console.WriteLine("Number of Search Strings that will be generated: " + NumberOfSearchCharsToGenerate.ToString("#,##0"));
+            Console.WriteLine();
+
+            object lockObject = new object();
+            int total = 0;
+            DateTime end = DateTime.Now;
+            DateTime start = DateTime.Now;
+            //the strings to search
+            string[] ss = new string[NumberOfStringsToGenerate];
+            //the chars/strings to look for. We use both because we're testing some string methods too.
+            string[] sf = new string[NumberOfSearchCharsToGenerate];
+            //the count of each substring finding
+            int[] c = new int[sf.Length];
+
+            //Generate the string arrays
+            int z = 10;
+
+            Console.WriteLine("Generating strings to search.");
+            for (int x = 0; x < ss.Length; x++)
+            {
+                ss[x] = System.Web.Security.Membership.GeneratePassword(z, x % 5);
+                z += 1;
+                if (z > 25)
+                    z = 10;
+            }
+
+            //strings to search for
+            Console.WriteLine("Generating strings to search for.");
+            z = 2;
+            for (int x = 0; x < sf.Length; x++)
+            {
+                sf[x] = System.Web.Security.Membership.GeneratePassword(z, x % 2);
+                z += 1;
+                if (z > 8)
+                    z = 2;
+            }
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            Console.WriteLine("T3 Starting method: 'String.Contains' ");
+            start = DateTime.Now;
+            for (int x = 0; x < ss.Length; x++)
+            {
+                for (int y = 0; y < sf.Length; y++)
+                {
+                    c[y] += (ss[x].Contains(sf[y]) == true ? 1 : 0);
+                }
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            Array.Clear(c, 0, c.Length);
+            Console.WriteLine("T1 Starting method: 'custom string method' ");
+            start = DateTime.Now;
+            for (int x = 0; x < ss.Length; x++)
+            {
+                for (int y = 0; y < sf.Length; y++)
+                {
+                    c[y] += ((ss[x].Length - ss[x].Replace(sf[y], String.Empty).Length) / sf[y].Length > 0 ? 1 : 0);
+                }
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+            Array.Clear(c, 0, c.Length);
+
+            Console.WriteLine("Starting method: 'count split string on string' ");
+            start = DateTime.Now;
+            for (int x = 0; x < ss.Length; x++)
+            {
+                for (int y = 0; y < sf.Length; y++)
+                {
+                    c[y] += (ss[x].Split(new string[] { sf[y] }, StringSplitOptions.None).Count() - 1 > 0 ? 1 : 0);
+                }
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+            Array.Clear(c, 0, c.Length);
+
+            Console.WriteLine("Starting method: 'String.indexOf' ");
+            start = DateTime.Now;
+            for (int x = 0; x < ss.Length; x++)
+            {
+                for (int y = 0; y < sf.Length; y++)
+                {
+                    c[y] += (ss[x].IndexOf(sf[y]) >= 0 ? 1 : 0);
+                }
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            Array.Clear(c, 0, c.Length);
+            Console.WriteLine("Starting method: 'linq contains usage' ");
+            start = DateTime.Now;
+            for (int y = 0; y < sf.Length; y++)
+            {
+                c[y] += ss.Where(o => o.Contains(sf[y])).Count();
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            Array.Clear(c, 0, c.Length);
+            Console.WriteLine("Starting method: 'linq with IndexOf usage' ");
+            start = DateTime.Now;
+            for (int y = 0; y < sf.Length; y++)
+            {
+                c[y] += ss.Where(o => o.IndexOf(sf[y]) > -1).Count();
+            }
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            total = 0;
+            for (int x = 0; x < c.Length; x++)
+            {
+                total += c[x];
+            }
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            Array.Clear(c, 0, c.Length);
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For Custom Counting' ");
+            start = DateTime.Now;
+            Parallel.For(0, ss.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    for (int y = 0; y < sf.Length; y++)
+                    {
+                        subtotal += ((ss[x].Length - ss[x].Replace(sf[y], String.Empty).Length) / sf[y].Length > 0 ? 1 : 0);
+                    }
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For Split String' ");
+            start = DateTime.Now;
+            Parallel.For(0, ss.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    for (int y = 0; y < sf.Length; y++)
+                    {
+                        subtotal += (ss[x].Split(new string[] { sf[y] }, StringSplitOptions.None).Count() - 1 > 0 ? 1 : 0);
+                    }
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For String.Contains()' ");
+            start = DateTime.Now;
+            Parallel.For(0, ss.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    for (int y = 0; y < sf.Length; y++)
+                    {
+                        subtotal += (ss[x].Contains(sf[y]) == true ? 1 : 0);
+                    }
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For String.IndexOf()' ");
+            start = DateTime.Now;
+            Parallel.For(0, ss.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    for (int y = 0; y < sf.Length; y++)
+                    {
+                        subtotal += (ss[x].IndexOf(sf[y]) >= 0 ? 1 : 0);
+                    }
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For Linq.Contains()' ");
+            start = DateTime.Now;
+            Parallel.For(0, sf.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    subtotal += ss.Where(o => o.Contains(sf[x])).Count();
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+            total = 0;
+            Console.WriteLine("Starting method: 'Parallel For Linq.IndexOf()' ");
+            start = DateTime.Now;
+            Parallel.For(0, sf.Length,
+                () => 0,
+                (x, loopState, subtotal) =>
+                {
+                    subtotal += ss.Where(o => o.IndexOf(sf[x]) > -1).Count();
+                    return subtotal;
+                },
+                (s) =>
+                {
+                    lock (lockObject)
+                    {
+                        total += s;
+                    }
+                }
+            );
+            end = DateTime.Now;
+            Console.WriteLine("Finished at: " + end.ToLongTimeString());
+            Console.WriteLine("Time: " + (end - start));
+            Console.WriteLine("Total finds: " + total + Environment.NewLine);
+            Console.WriteLine();
+            Console.WriteLine("###########################################################");
+            Console.WriteLine();
+
+
+
+            Array.Clear(ss, 0, ss.Length);
+            ss = null;
+            Array.Clear(sf, 0, sf.Length);
+            sf = null;
+            Array.Clear(c, 0, c.Length);
+            c = null;
+            GC.Collect();
+
+        }
+    }
+
+}
